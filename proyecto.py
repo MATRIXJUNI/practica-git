11b7452df8b032ea49d9ce40f0243f192c7c3541
esteban <escastr@gmail.com>
2023-02-02 18:44:04 -0500

    ejemplo condicionales

--- proyecto.py
+++ proyecto.py
@@ -1,2 +1,14 @@
 print("bienvenido")
 #practico git y hago la tarea al tiempo
+
+#temas vistos hoy condicinoal,bucles,diccionarios
+#tarea : hacer un traductor
+
+edad = int(input(" digite su edad : "))
+if edad <= 17 :
+    print("eres menor de edad")
+elif edad >= 18 and edad <=150 :
+    print("eres mayor ")
+
+else :
+    print("esa edad no es valida")
